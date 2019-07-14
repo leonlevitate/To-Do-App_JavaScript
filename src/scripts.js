0eaf5beedb8d0ec05864506b719934719f6f7796
Leon Linton <leon.linton@live.co.uk>
2019-07-14 18:57:42 +0100

    First pass at adding close button

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,5 @@
+var closeButton = document.getElementsByClassName("close");
+
 function createNewElement() {
     var li = document.createElement('li');
     var theInputValue = document.getElementById("input").value;
@@ -11,4 +13,23 @@ function createNewElement() {
         document.getElementById("the-ul").appendChild(li);
     }
 
+    document.getElementById("input").value = "";
+
+    var theSpanTag = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    theSpanTag.className = "close";
+    theSpanTag.appendChild(text);
+    li.appendChild(theSpanTag);
+
+// Remove items when click on SPAN CLOSE BUTTON
+
+    for(i = 0; i < closeButton.length; i++){
+        closeButton[i].onclick = function(){
+            var theDiv = this.parentElement;
+            theDiv.style.display = "none"
+        }
+    }
+
 }
+
+createNewElement();
