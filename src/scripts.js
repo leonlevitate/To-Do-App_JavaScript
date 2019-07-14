f8c5ded085d33b026fbb9577436ffbf1abb203e6
Leon Linton <leon.linton@live.co.uk>
2019-07-14 18:18:27 +0100

    Add JS function for creating new list item element

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,14 @@
-console.log("Works")
+function createNewElement() {
+    var li = document.createElement('li');
+    var theInputValue = document.getElementById("input").value;
+    var textNode = document.createTextNode(theInputValue);
+    
+    li.appendChild(textNode);
+
+    if(theInputValue === '') {
+        alert("Please enter your note")
+    } else {
+        document.getElementById("the-ul").appendChild(li);
+    }
+
+}
